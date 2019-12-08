6cc80192aa18696c6b66413174fbdfd43c9f0225
Tyler Wellington <twellin1@binghamton.edu>
2019-12-07 22:54:10 -0500

    ship moves laterally with touch

--- cs441_Project7/GameScene.swift
+++ cs441_Project7/GameScene.swift
@@ -18,74 +18,76 @@ class GameScene: SKScene {
         }
     }
     
-    let gameBG = SKSpriteNode(imageNamed: "space")
+    //let gameBG = SKSpriteNode(imageNamed: "space")
     let planet = SKSpriteNode(imageNamed: "planet")
     let ship = SKSpriteNode(imageNamed: "ship")
     let laser = SKSpriteNode(imageNamed: "laser")
+    var touchLocation = CGPoint()
     
     override func didMove(to view: SKView) {
         //gameBG.position = CGPoint(x: size.width * 0.2, y: size.width * 0.1)
-        gameBG.position = CGPoint(x: 75, y: 0)
-        gameBG.zPosition = -1
-        gameBG.name = "gameBG"
-        addChild(gameBG)
+//        gameBG.position = CGPoint(x: 75, y: 0)
+//        gameBG.zPosition = -1
+//        gameBG.name = "gameBG"
+//        addChild(gameBG)
+        run(SKAction.repeatForever(SKAction.sequence([SKAction.run(moveBackground), SKAction.wait(forDuration: 0.75)])))
         
         planet.position = CGPoint(x: 110, y: 450)
         planet.zPosition = -1
         planet.name = "planet"
         addChild(planet)
         
-        ship.position = CGPoint(x: 0, y : -550)
+        ship.position = CGPoint(x: 0, y : -500)
         ship.zPosition = 0
         ship.name = "ship"
         addChild(ship)
         
-        laser.position = CGPoint(x: 0, y: -495)
+        laser.position = CGPoint(x: 0, y: -445)
         addChild(laser)
+    }
+    
+    func moveBackground(){
+        let gameBG1 = SKSpriteNode(imageNamed: "space")
+        //let gameBG2 = SKSpriteNode(imageNamed: "space")
         
-//        // Get label node from scene and store it for use later
-//        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-//        if let label = self.label {
-//            label.alpha = 0.0
-//            label.run(SKAction.fadeIn(withDuration: 2.0))
-//        }
-//
-//        // Create shape node to use during mouse interaction
-//        let w = (self.size.width + self.size.height) * 0.05
-//        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
-//
-//        if let spinnyNode = self.spinnyNode {
-//            spinnyNode.lineWidth = 2.5
-//
-//            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-//            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-//                                              SKAction.fadeOut(withDuration: 0.5),
-//                                              SKAction.removeFromParent()]))
-//        }
+        gameBG1.zPosition = -2
+        gameBG1.position = CGPoint(x: 75, y: 0)
+        //gameBG2.zPosition = -1
+        //gameBG2.position = CGPoint(x: 75, y: 500)
+        addChild(gameBG1)
+        //addChild(gameBG2)
+        
+        let duration = CGFloat(4.0)
+        let move = SKAction.move(to: CGPoint(x: 75, y: -7500), duration: TimeInterval(duration))
+        let finish = SKAction.removeFromParent()
+        gameBG1.run(SKAction.sequence([move, finish]))
     }
     
-    
-    func touchDown(atPoint pos : CGPoint) {
+    func moveShip(){
+        ship.position.x = (touchLocation.x)
     }
     
-    func touchMoved(toPoint pos : CGPoint) {
+    func shootLaser(){
+        //let shoot = SKAction.mo
     }
     
-    func touchUp(atPoint pos : CGPoint) {
+    func spawnEnemies(){
+        
     }
     
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-    }
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {}
     
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for touch in touches{
+            touchLocation = touch.location(in: self)
+            
+            moveShip()
+        }
     }
     
-    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-    }
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {}
     
-    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-    }
-    
+    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {}
     
     override func update(_ currentTime: TimeInterval) {
         // Called before each frame is rendered
